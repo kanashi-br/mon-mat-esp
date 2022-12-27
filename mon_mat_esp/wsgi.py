dd6dbbd40004440fa5009d263c1f73934cc8e5e4
Marlison S. S <marlisonsoaresjp@gmail.com>
2022-12-26 21:28:34 -0300

    cliente_suap_django -> mon_mat_esp

--- mon_mat_esp/wsgi.py
+++ mon_mat_esp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for cliente_suap_django project.
+WSGI config for mon_mat_esp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cliente_suap_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mon_mat_esp.settings')
 
 application = get_wsgi_application()
